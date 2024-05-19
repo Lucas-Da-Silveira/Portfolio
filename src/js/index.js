9aad09ba8ddf25af08bd49d376ae9d7f9876ca2f
Lucas <lucas.da_silveira@edu.univ-fcomte.fr>
2024-05-19 17:37:50 +0200

    Premier commit

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+window.onscroll = function () {
+    const backToTopButton = document.getElementById('backToTop');
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+};
+
+function scrollToTop() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
